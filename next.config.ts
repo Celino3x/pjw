36737149c2534949e1c0fbdee991376f531cf915
Celino3x <celino3x@gmail.com>
2026-07-29 15:36:02 -0300

    Corrigindo next.config.ts para Next.js 16

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
-    domains: ['localhost'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
 };
 
